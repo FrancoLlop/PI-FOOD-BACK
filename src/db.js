90e72f6c6a838bfec637d01e43b1bf0336094709
Franco <francollopard@gmail.com>
2023-08-16 15:57:44 -0300

    cambios db

--- src/db.js
+++ src/db.js
@@ -14,6 +14,11 @@ const {
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  }
 });
 
 const basename = path.basename(__filename);
